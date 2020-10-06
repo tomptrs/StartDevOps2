dca1bb354cef7762cc2fd265d406eed361883806
Tom Peeters <tom_ptrs@yahoo.com>
2020-10-06 10:48:23 +0200

    start project

---
+++ StartDevops2/StartDevops2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StartDevops2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
